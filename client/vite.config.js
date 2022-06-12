7bb9c98d412ce23c4b7f7ae622b2abb7dace6674
Sunday Nkwuda <sonnietech@disroot.org>
2022-06-12 01:09:02 +0100

    add config for styled-components

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-styled-components",
+            {
+              meaninglessFileNames: ["index", "styles"],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+});
