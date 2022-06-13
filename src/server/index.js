2b733574256b8fe43c6609ee98de2fde96288c7a
Sunday Nkwuda <sonnietech@disroot.org>
2022-06-13 13:40:54 +0100

    merge server and client to single package, use concurrently

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,12 @@ const { PORT = 3001 } = process.env;
 
 app.use(express.json());
 
-app.use(express.static("dist/app"));
+app.use(express.static("dist/client"));
 
 app.use("/api/v1", (req, res) => res.status(200).send("accessed api"));
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "app/index.html"));
+  res.sendFile(path.join(__dirname, "client/index.html"));
 });
 
 app.listen(PORT, () => {
